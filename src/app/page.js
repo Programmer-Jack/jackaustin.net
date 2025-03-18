28622820898c2c6e53debc00046906b8849af496
Jack Austin <jack@jackaustin.net>
2025-03-17 21:21:00 -0600

    Update colors

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
+      <main>
         <h1>// I AM JACK AUSTIN</h1>
         <section>
           <h2>
