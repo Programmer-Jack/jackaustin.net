5190088b53abe94d882ac657748a6cbaeb3ea886
Jack Austin <jack@jackaustin.net>
2025-03-17 19:53:06 -0600

    New designs

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 // import localFont from "next/font/local";
 import styles from "./globals.css";
+import Link from "next/link"
 
 // const geistSans = localFont({
 //   src: "./fonts/GeistVF.woff",
@@ -20,28 +21,33 @@ import styles from "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8"></meta>
+        <meta name="description" content="You know what time it is."></meta>
+        <meta name="keywords" content="Developer, Games, Unity, Indie"></meta>
+        <meta name="author" content="Jack Austin"></meta>
+        <title>Jack Austin</title>
+      </head>
       <body>
-        <main>
-          <header>
-            <a
-              href="/"
-              rel="nooper noreferrer">
-              Home
-            </a>
-            <a
-              href="/portfolio"
-              rel="nooper noreferrer"
-              >
-             Portfolio
-            </a>
-          </header>
-          {children}
-          {/* <footer>
-            <div className="footer">
-            Lorem ipsum odor amet, consectetuer adipiscing elit. Aliquam vel porttitor gravida lobortis augue magna erat. Tristique aptent nisl vel platea ligula habitasse facilisi. Vivamus posuere urna suspendisse massa egestas; felis platea ac. Non mus dis bibendum suspendisse arcu odio, posuere magnis. Imperdiet vestibulum cras elementum sit velit ridiculus nisi mattis.
-            </div> 
-          </footer> */}
-        </main>
+        <header>
+          <Link
+            href="/"
+            rel="nooper noreferrer">
+            Home
+          </Link>
+          <Link
+            href="/portfolio"
+            rel="nooper noreferrer"
+            >
+            Portfolio
+          </Link>
+        </header>
+        {children}
+        {/* <footer>
+          <div className="footer">
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Aliquam vel porttitor gravida lobortis augue magna erat. Tristique aptent nisl vel platea ligula habitasse facilisi. Vivamus posuere urna suspendisse massa egestas; felis platea ac. Non mus dis bibendum suspendisse arcu odio, posuere magnis. Imperdiet vestibulum cras elementum sit velit ridiculus nisi mattis.
+          </div> 
+        </footer> */}
       </body>
     </html>
   );
