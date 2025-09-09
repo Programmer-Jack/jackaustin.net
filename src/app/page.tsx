83128b868566a69c5f3aa8c207f38677a4f4dec6
Jack Austin <jack@jackaustin.net>
2025-09-09 19:51:37 -0400

    Update links, icons, photos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,35 @@
 import React from "react";
-import Image from "next/image";
+// import Image from "next/image";
 // import styles from "./page.module.css";
 
-interface LightDarkSocialIconProps {
-	lightSrc: string;
-	darkSrc: string;
-	alt: string;
-}
+// interface LightDarkSocialIconProps {
+// 	lightSrc: string;
+// 	darkSrc: string;
+// 	alt: string;
+// }
 
-function LightDarkSocialIcon({ lightSrc, darkSrc, alt }: LightDarkSocialIconProps): React.JSX.Element {
-	const isDark = typeof window !== "undefined"
-		? window.matchMedia("(prefers-color-scheme: dark)").matches
-		: true;
-	const imgSrc = isDark ? darkSrc : lightSrc;
+// function LightDarkSocialIcon({ lightSrc, darkSrc, alt }: LightDarkSocialIconProps): React.JSX.Element {
+// 	const isDark = typeof window !== "undefined"
+// 		? window.matchMedia("(prefers-color-scheme: dark)").matches
+// 		: true;
+// 	const imgSrc = isDark ? darkSrc : lightSrc;
 
-	return (
-		// <Image
-		// 	src={imgSrc}
-		// 	alt={alt}
-		// 	fill={true}
-		// />
-		<img src={imgSrc}
-			 alt={alt}
-		></img>
-	);
-}
+// 	return (
+// 		<img src={imgSrc}
+// 			 alt={alt}
+// 		></img>
+// 	);
+// }
 
-const itchLight = "/ItchPressKit/logos/itchio-logo-black.svg"
+// const itchLight = "/ItchPressKit/logos/itchio-logo-black.svg"
 const itchDark = "/ItchPressKit/logos/app-icon.svg"
-const liInLight = "/in-logo/InBug-Black.png"
+// const liInLight = "/in-logo/InBug-Black.png"
 const liInDark = "/in-logo/InBug-White.png"
-const gitHubLight = "/GitHubLogos/SVG/GitHub_Invertocat_Dark.svg"
+// const gitHubLight = "/GitHubLogos/SVG/GitHub_Invertocat_Dark.svg"
 const gitHubDark = "/GitHubLogos/SVG/GitHub_Invertocat_Light.svg"
-const igLight = "/IGPressKit/03 Black Glyph/Instagram_Glyph_Black.svg"
-const igDark = "/IGPressKit/02 White Glyph/Instagram_Glyph_White.svg";
-const bskyLight = "/BlueskyLogos/bluesky_media_kit_logo.svg";
+// const igLight = "/IGPressKit/03 Black Glyph/Instagram_Glyph_Black.svg"
+const igDark = "/IGPressKit/01 Gradient Glyph/Instagram_Glyph_Gradient.svg";
+// const bskyLight = "/BlueskyLogos/bluesky_media_kit_logo.svg";
 const bskyDark = "/BlueskyLogos/bluesky_media_kit_logo_3.svg"
 
 export default function Home() {
@@ -51,48 +46,63 @@ export default function Home() {
 				<h3>Follow me</h3>
 				<div className="socials-grid">
 					<div className="social-icon">
-						<a href="https://jackaustin.itch.io/">
-							<LightDarkSocialIcon
+						<a rel="noopener noreferrer" href="https://jackaustin.itch.io/">
+							{/* <LightDarkSocialIcon
 								lightSrc={itchLight}
 								darkSrc={itchDark}
 								alt="itch.io"
-							/>
+							/> */}
+							<img src={itchDark}
+								 alt="itch.io"
+							></img>
 						</a>
 					</div>
 					<div className="social-icon">
-						<a href="https://www.linkedin.com/in/iamjackaustin/">
-							<LightDarkSocialIcon
+						<a rel="noopener noreferrer" href="https://www.linkedin.com/in/iamjackaustin/">
+							{/* <LightDarkSocialIcon
 								lightSrc={liInLight}
 								darkSrc={liInDark}
 								alt="LinkedIn"
-							/>
+							/> */}
+							<img src={liInDark}
+								 alt="LinkedIn"
+							></img>
 						</a>
 					</div>
 					<div className="social-icon">
-						<a href="https://github.com/Programmer-Jack">
-							<LightDarkSocialIcon
+						<a rel="noopener noreferrer" href="https://github.com/Programmer-Jack">
+							{/* <LightDarkSocialIcon
 								lightSrc={gitHubLight}
 								darkSrc={gitHubDark}
 								alt="GitHub"
-							/>
+							/> */}
+							<img src={gitHubDark}
+								 alt="GitHub"
+							></img>
 						</a>
 					</div>
 					<div className="social-icon">
-						<a href="https://www.instagram.com/_programmer_jack/">
-							<LightDarkSocialIcon
+						<a rel="noopener noreferrer" href="https://www.instagram.com/_programmer_jack/">
+							{/* <LightDarkSocialIcon
 								lightSrc={igLight}
 								darkSrc={igDark}
 								alt="Instagram"
-							/>
+							/> */}
+							<img src={igDark}
+								 alt="Instagram"
+							></img>
 						</a>
 					</div>
 					<div className="social-icon">
-						<a href="https://bsky.app/profile/jackaustin.net">
-							<LightDarkSocialIcon
+						<a rel="noopener noreferrer" href="https://bsky.app/profile/jackaustin.net">
+							{/* <LightDarkSocialIcon
 								lightSrc={bskyLight}
 								darkSrc={bskyDark}
 								alt="bsky"
-							/>
+							/> */}
+							<img src={bskyDark}
+								 alt="bsky"
+							></img>
 						</a>
 					</div>
 				</div>
@@ -100,17 +110,27 @@ export default function Home() {
 			<section>
 				<h3>Gallery</h3>
 				<div className="photo-gallery-container">
+					<a rel="noopener noreferrer" href="https://jackaustin.itch.io/vector-c">
+						<img
+							src="/games/vector-c.png"
+							alt="Vector-C, a two-player, arcade-style, top-down shooter."
+						></img>
+					</a>
+					<a rel="noopener noreferrer" href="https://jackaustin.itch.io/portal-warpin-robot-killer">
+						<img
+							src="/games/portal_robot_0.png"
+							alt="A brightly glowing orange portal down a concrete corridor"
+						></img>
+					</a>
+					<a rel="noopener noreferrer" href="https://jackaustin.itch.io/space-western">
+						<img
+							src="/games/space_western.png"
+							alt="An inventory lit up by the desert sun"
+						></img>
+					</a>
 					<img
-					src="/games/the_room.png"
-					alt="A quiet storage room at night, illuminated by a lamp"
-					></img>
-					<img
-						src="/games/space_western.png"
-						alt="An inventory lit up by the desert sun"
-					></img>
-					<img
-						src="/games/portal_robot_0.png"
-						alt="A brightly glowing orange portal down a concrete corridor"
+						src="/games/the_room.png"
+						alt="A quiet storage room at night, illuminated by a lamp"
 					></img>
 				</div>
 			</section>
