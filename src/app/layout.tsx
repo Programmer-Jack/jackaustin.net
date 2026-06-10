c7cc4b2359efdd2398f265c696830306b79a312f
Jack Austin <jack@jackaustin.net>
2026-06-10 19:32:52 -0400

    New look

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,16 @@ export default function RootLayout({
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css"/>
       </head>
       <body>
+        <header className="title">
+				  <h1>Jack Austin</h1>
+          <p><em>makes games, not websites!</em></p>
+        </header>
+        <section>
+          <nav className="header-menu">
+            <a href="/">/</a>
+            <a href="/portal-warpin-robot-killer">/portal-warpin-robot-killer</a>
+          </nav>
+        </section>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,27 @@ const itchDark = "/ItchPressKit/logos/app-icon.svg"
 const liInDark = "/in-logo/InBug-White.png"
 const gitHubDark = "/GitHubLogos/SVG/GitHub_Invertocat_Light.svg"
 const igDark = "/ig/gradient/Instagram_Glyph_Gradient.png";
-const bskyDark = "/BlueskyLogos/bluesky_media_kit_logo_3.svg"
+const bskyDark = "/bsky/bluesky_media_kit_logo_3.svg"
+const ytIcon = "/youtube-icon/01-red/yt_icon_red_digital.png"
 
 export default function Home() {
 	return (
 		<main>
-			<section className="title">
-				<h1>Jack Austin</h1>
-				<h2>/ Game developer</h2>
-			</section>
 			<section>
-				<p>I&apos;m passionate about building games. / Games are art. / The best games make us want to spend more time exploring the real world. / Games tell stories. / We&apos;ve been telling stories since the beginning of time.</p>
+				<h3>Latest tutorial</h3>
+				{/* 
+					Source - https://stackoverflow.com/a/20130011
+					Posted by Simone Lazzari, modified by community. See post 'Timeline' for change history
+					Retrieved 2026-06-10, License - CC BY-SA 4.0
+				*/}
+				<div className="wrapper">
+					<div className="h_iframe">
+						<iframe width={2} height={2} src="https://www.youtube.com/embed/TdvbNz5dkeI?si=Oz8ShNa0j97fgMzl" title="YouTube video player" frameBorder="0"
+								allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+								referrerPolicy="strict-origin-when-cross-origin" allowFullScreen>
+						</iframe>
+					</div>
+				</div>
 			</section>
 			<section className="socials-section">
 				<h3>Follow me</h3>
@@ -23,35 +33,42 @@ export default function Home() {
 						<a rel="noopener noreferrer" href="https://jackaustin.itch.io/">
 							<img src={itchDark}
 								alt="itch.io"
-							></img>
+							/>
 						</a>
 					</div>
 					<div className="social-icon">
 						<a rel="noopener noreferrer" href="https://www.linkedin.com/in/iamjackaustin/">
 							<img src={liInDark}
 								alt="LinkedIn"
-							></img>
+							/>
 						</a>
 					</div>
 					<div className="social-icon">
 						<a rel="noopener noreferrer" href="https://github.com/Programmer-Jack">
 							<img src={gitHubDark}
 								alt="GitHub"
-							></img>
+							/>
 						</a>
 					</div>
 					<div className="social-icon">
 						<a rel="noopener noreferrer" href="https://www.instagram.com/_programmer_jack/">
 							<img src={igDark}
 								alt="Instagram"
-							></img>
+							/>
 						</a>
 					</div>
 					<div className="social-icon">
 						<a rel="noopener noreferrer" href="https://bsky.app/profile/jackaustin.net">
 							<img src={bskyDark}
 								alt="bsky"
-							></img>
+							/>
+						</a>
+					</div>
+					<div className="social-icon">
+						<a rel="noopener noreferrer" href="https://www.youtube.com/@iamjackaustin">
+							<img src={ytIcon}
+								alt="YouTube"
+							/>
 						</a>
 					</div>
 				</div>
@@ -63,24 +80,24 @@ export default function Home() {
 						<img
 							src="/games/vector-c.png"
 							alt="Vector-C, a two-player, arcade-style, top-down shooter."
-						></img>
+						/>
 					</a>
 					<a rel="noopener noreferrer" href="https://jackaustin.itch.io/portal-warpin-robot-killer">
 						<img
 							src="/games/portal_robot_0.png"
 							alt="A brightly glowing orange portal down a concrete corridor"
-						></img>
+						/>
 					</a>
 					<a rel="noopener noreferrer" href="https://jackaustin.itch.io/space-western">
 						<img
 							src="/games/space_western.png"
 							alt="An inventory lit up by the desert sun"
-						></img>
+						/>
 					</a>
 					<img
 						src="/games/the_room.png"
 						alt="A quiet storage room at night, illuminated by a lamp"
-					></img>
+					/>
 				</div>
 			</section>
 		</main>
